ebda763b4d64f9578cdfc12c3f9bb071846f18cd
Valmir Júnior <valmirosjunior@gmail.com>
2026-01-20 01:45:33 -0300

    test: add API request specs

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  # Disable host authorization in test environment.
+  config.hosts.clear
+
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
 
